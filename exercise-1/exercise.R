2c0ffd1a7608795a49fc20a2b80cf9fb2e8f46a5
Kelly Chhor <kcc343@gmail.com>
2018-04-03 16:58:32 -0700

    add my name to names vector

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,7 +3,7 @@
 # Create a vector `names` that contains your name and the names of 2 people 
 # next to you. Print the vector.
 
-names <- c("Heidi", "Nancy")
+names <- c("Kelly", "Heidi", "Nancy")
 print(names)
 
 # Use the colon operator : to create a vector `n` of numbers from 10:49
